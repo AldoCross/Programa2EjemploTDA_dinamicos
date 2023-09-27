92177490965f39f461bf09d9e266220d9ceb594b
Aldo Cruz Sosa <112133794+AldoCross@users.noreply.github.com>
2023-09-27 11:11:07 -0700

    Add files via upload

---
+++ programa2-ejemplo TDA. (dinámicos)/Tarea 1.2 TDA/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections; //libreria necesaria
+
+namespace programa2_ejemplo_TDA_dinámicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor;
+            Stack pila = new Stack();
+            // ingreso de elementos a la pila:
+            pila.Push(" Primero ");
+            pila.Push(" Segundo ");
+            pila.Push(" Tercero ");
+            foreach (string var in pila)
+            {
+                Console.WriteLine(var);
+            }
+            Console.WriteLine("\n");
+            // Peek retorna el valor que esta al tope de la pila
+            Console.Write(" El Tope de la Pila es: ");
+            Console.WriteLine(pila.Peek());
+            //retorna el valor del tope eliminandolo:
+            valor = pila.Pop().ToString();
+            Console.WriteLine(" Eliminado Pila... " + valor);
+            Console.WriteLine("\n");
+            // mostrar contenido de la pila:
+            foreach(string var in pila)
+            {
+                Console.WriteLine(var);
+            }
+            Console.ReadKey();
+        }
+    }
+}
